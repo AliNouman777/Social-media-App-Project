97a5d2b9a7f2bd7d876689bf288222fbbcb7ceb9
Ali Nouman <129924156+AliNouman777@users.noreply.github.com>
2025-04-10 08:06:13 -0700

    Update ConnectDB.js

--- Backend/Database/ConnectDB.js
+++ Backend/Database/ConnectDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const  ConnectDb= ()=>{
-    mongoose.connect("mongodb+srv://admin:micro786@practice.9ljftft.mongodb.net/SocialMediaApp?retryWrites=true&w=majority").then(()=>{
+    mongoose.connect("mongodb+srv://ali:12345@cluster0.awg30xs.mongodb.net/SocialMediaApp?retryWrites=true&w=majority&tls=true").then(()=>{
     console.log("connected to database")
     }).catch((error)=>{
       return  console.log("error connecting to database")
